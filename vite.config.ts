08451e6de34f06e0fbda778c38a9366f58ccd048
vtmhieu <vtmhieu111@gmail.com>
2025-11-28 12:06:21 +0100

    remove lovable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,7 +15,7 @@ export default defineConfig(({ mode }) => {
       port: 8080,
     },
     
-    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    plugins: [react()],
     
     resolve: {
       alias: {
